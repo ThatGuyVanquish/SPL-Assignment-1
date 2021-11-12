52abde1bd818d6bf5488c3a29d4bb7a959e36ccf
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2021-11-12 14:53:42 +0200

    Update Workout.cpp
    Cosmetics

--- base_code/Workout.cpp
+++ base_code/Workout.cpp
@@ -3,13 +3,18 @@
 
 using namespace std;
 
+// Constructor
 Workout::Workout(int w_id, std::string w_name, int w_price, WorkoutType w_type):
     id(w_id), 
     name(w_name), 
     price(w_price), 
     type(w_type)
-    {}; // Constructor
-    
+    {}; 
+
+/*
+**** Getters ****
+*/
+
 int Workout::getId() const {
     return id;
 };
