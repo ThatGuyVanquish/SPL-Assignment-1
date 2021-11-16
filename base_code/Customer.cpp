aa18730bb8524d4e5a9d0bf01eaca1fc83cd5cc5
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2021-11-16 20:04:32 +0200

    Update Customer.cpp
    Sort might cause problems because of weird workout vector copy (caused issues to emi and lior)

--- base_code/Customer.cpp
+++ base_code/Customer.cpp
@@ -126,7 +126,7 @@ std::vector<int> HeavyMuscleCustomer::order(const std::vector<Workout>& workoutO
 {
     std::vector<int> wrk;
     std::vector<Workout> wrkCOpy(workoutOptions);
-    std::sort(wrkCOpy.begin(), wrkCOpy.end(), compareAnae);
+    std::sort(wrkCOpy.begin(), wrkCOpy.end(), compareAnae); // Caused problems for emi
     for (Workout workout : wrkCOpy)
     { // Might be able to use else to break for loop if we created the comparator properly.
         if (workout.getType() == ANAEROBIC)
